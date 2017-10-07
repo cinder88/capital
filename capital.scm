02f9cdfe7a5a0edf0cfa4e419da7141b49288d00
ate <andrew.erlanger@gmail.com>
2017-10-07 11:52:03 -0400

    simplified a lot, added balance function, removed account class

--- capital.scm
+++ capital.scm
@@ -1,38 +1,29 @@
 (use-modules (ice-9 optargs)
+			 (srfi srfi-1)
 			 (srfi srfi-19)
 			 (oop goops))
 
 (define-class <unit> ()
-  (symbol #:init-keyword #:symbol))
+  (symbol #:init-form 'USD
+		  #:init-keyword #:symbol))
+
+(define *default-unit* (make <unit>))
 
 (define-class <capital> ()
   (scalar #:init-keyword #:scalar
 		  #:accessor scalar)
-  (unit #:init-keyword #:unit
-		#:accessor unit))
+  (unit #:init-form *default-unit*
+	    #:init-keyword #:unit
+		#:getter unit))
 
-(define-class <account> ()
-  (name #:init-keyword #:symbol)
-  (capital #:init-keyword #:capital #:accessor capital)
-  (transactions #:init-form '() #:accessor transactions))
-
-(define-class <transaction> ()
-  (date #:init-keyword #:date
-		#:accessor date)
-  (from-account #:init-keyword #:from-account
-				#:accessor from-account)
-  (to-account #:init-keyword #:to-account
-			  #:accessor to-account)
-  (capital #:init-keyword #:capital
-		   #:accessor capital))
-
-(define-class <ledger> ()
-  (accounts #:init-form '() #:accessor accounts)
-  (transactions #:init-form '() #:accessor transactions))
-
+(define-method (equal? (c1 <capital>) (c2 <capital>))
+  (and (equal? (scalar c1) (scalar c2))
+	   (equal? (unit c1) (unit c2))))  
 
 (define-method (- (x <number>) (C <capital>))
   (make <capital> #:unit (unit C) #:scalar (- (scalar C))))
+
+(define-method (+ (C1 <capital>)) C1)
 
 (define-method (+ (C1 <capital>) (C2 <capital>))
   (make <capital>
@@ -44,46 +35,52 @@
 	#:unit (unit C1)
 	#:scalar (+ (scalar C1) (- (scalar C2)))))
 
+(define-method (- (C <capital>) (x <number>)) C)
 
-(define-method (add-capital (C <capital>) (A <account>))
-  (set! (capital A) (+ (capital A) C)))
+(define-class <transaction> ()
+  (date #:init-keyword #:date
+		#:accessor date)
+  (from-account #:init-keyword #:from-account
+				#:accessor from-account)
+  (to-account #:init-keyword #:to-account
+			  #:accessor to-account)
+  (capital #:init-keyword #:capital
+			  #:accessor capital))
 
-(define-method (remove-capital (C <capital>) (A <account>))
-  (set! (capital A) (- (capital A) C)))
+(define-class <ledger> ()
+  (transactions #:init-form '() #:accessor transactions)
+  (accounts #:init-form '() #:accessor accounts)
+  (account-strict #:init-form #f #:accessor account-strict?))
 
-(define-method (process-transaction (T <transaction>) (A <account>))
-  (cond
-   ((eq? A (from-account T))
-	(set! (transactions A) (cons T (transactions A)))
-	(remove-capital (capital T) A))
-   ((eq? A (to-account T))
-	(set! (transactions A) (cons T (transactions A)))
-	(add-capital (capital T) A))
-   ((#t))))
-	
+(define-method (+ (L <ledger>) (acct <symbol>))
+  (set! (accounts L) (delete-duplicates
+					  (cons acct (accounts L)))))
 
-(define-method (add-account (A <account>) (L <ledger>))
-  (set! (accounts L) (cons A (accounts L))))
+(define-method (+ (L <ledger>) (t <transaction>))
+  (let ((from (from-account t))
+		(to (to-account t)))
+	(cond ((account-strict? L)
+		   (if (not (and (memq from L)
+						 (memq to L)))
+			   (throw 'unknown-acount)))
+		  (#t
+		   (+ L from)
+		   (+ L to))))
+  (set! (transactions L) (cons t (transactions L)))
+  #t)
 
-(define-method (add-transaction (T <transaction>) (L <ledger>))  
-  (map
-   (lambda (acct) (if (not (memq acct (accounts L)))
-					  (add-account acct L)))
-   (list (from-account T) (to-account T)))
-  (map (lambda (acct) (process-transaction T acct))
-	   (accounts L))
-  (set! (transactions L) (cons T (transactions L))))
+(define-method (transactions (account <symbol>) (L <ledger>))
+  (filter (lambda (t) (or (equal? (from-account t) account)
+						  (equal? (to-account t) account)))
+		  (transactions L)))
 
-
-;; (define (new-transaction from to capital
-;; 						 #:key (check-accounts #t))
-;;   (let ((new (make <transaction>
-;; 				#:from-account from
-;; 				#:to-account to
-;; 				#:capital capital)))
-;; 	(if (not
-;; 		 (throw 'unknown-account
-;; 	(set! %transaction (cons new %transactions))
-;; 	new))
-					
-					
+(define-method (balance (account <symbol>) (L <ledger>))
+  (let ((transaction-list (transactions account L)))
+	(let ((froms (filter (lambda (t) (equal? (from-account t)
+											 account))
+						 transaction-list))
+		  (tos (filter (lambda (t) (equal? (to-account t)
+										   account))
+					   transaction-list)))
+	  (- (apply + (map capital tos))
+	  	 (apply + (map capital froms))))))
