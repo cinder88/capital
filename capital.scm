f88ad04207b992bb213cf443bf6b1013acf829ef
ate <andrew.erlanger@gmail.com>
2017-10-08 12:59:46 -0400

    added the <asset> class
    the asset class is used for doing operations on pieces
    of capital which do not have the same unit. adding capital
    or another asset to an asset will attempt to consolidate the
    capital as much as possible inside of the asset. all algebraic
    operations which are possible on bits of capital are also
    applicable on assets

--- capital.scm
+++ capital.scm
@@ -4,38 +4,83 @@
 			 (oop goops))
 
 (define-class <unit> ()
-  (symbol #:init-form 'USD
-		  #:init-keyword #:symbol))
+  (name #:init-form 'USD
+		#:init-keyword #:name
+		#:getter name))
 
-(define *default-unit* (make <unit>))
+(define USD (make <unit> #:name 'USD))
+(define USD-V (make <unit> #:name 'USD-V))
 
 (define-class <capital> ()
   (scalar #:init-keyword #:scalar
 		  #:accessor scalar)
-  (unit #:init-form *default-unit*
-	    #:init-keyword #:unit
+  (unit #:init-form USD
+		#:init-keyword #:unit
 		#:getter unit))
 
+;; unary operators
 (define-method (equal? (c1 <capital>) (c2 <capital>))
   (and (equal? (scalar c1) (scalar c2))
-	   (equal? (unit c1) (unit c2))))  
+	   (equal? (unit c1) (unit c2))))
 
-(define-method (- (x <number>) (C <capital>))
-  (make <capital> #:unit (unit C) #:scalar (- (scalar C))))
+(define-method (- (x <number>) (c <capital>))
+  (make <capital> #:unit (unit c) #:scalar (- (scalar c))))
+(define-method (- (c <capital>))
+  (make <capital> #:unit (unit c) #:scalar (- (scalar c))))
 
-(define-method (+ (C1 <capital>)) C1)
+(define-method (+ (c1 <capital>)) c1)
 
-(define-method (+ (C1 <capital>) (C2 <capital>))
-  (make <capital>
-	#:unit (unit C1)
-	#:scalar (+ (scalar C1) (scalar C2))))
+(define-method (value (c <capital>))
+  (list (scalar c) (name (unit c))))
 
-(define-method (- (C1 <capital>) (C2 <capital>))
-  (make <capital>
-	#:unit (unit C1)
-	#:scalar (+ (scalar C1) (- (scalar C2)))))
+;; binary operators
+(define-method (+ (c1 <capital>) (c2 <capital>))
+  (if (equal? (unit c1) (unit c2))
+	  (make <capital>
+		#:unit (unit c1)
+		#:scalar (+ (scalar c1) (scalar c2)))
+	  (make <asset> #:capital (list c1 c2))))
 
-(define-method (- (C <capital>) (x <number>)) C)
+(define-method (- (c1 <capital>) (c2 <capital>))
+  (+ c1 (- c2)))
+
+(define-method (- (c <capital>) (x <number>)) c)
+
+
+(define-class <asset> ()
+  (capital #:init-form '()
+		   #:init-keyword #:capital
+		   #:accessor capital))
+
+;; unary operators
+(define-method (- (x <number>) (a <asset>))
+  (make <asset> #:capital (map - (capital a))))
+
+(define-method (+ (a <asset>)) a)
+
+(define-method (value (a <asset>))
+  (map value (capital a)))
+
+;; binary operators
+(define-method (+ (a <asset>) (c <capital>))
+  (define (add-recur list c)
+	(cond ((null? list) (list c))
+		  ((equal? (unit (car list)) (unit c))
+		   (cons (+ (car list) c) (cdr list)))
+		  (#t
+		   (cons (car list) (add-recur (cdr list) c)))))
+  (make <asset> #:capital (add-recur (capital a) c)))
+
+(define-method (+ (c <capital>) (a <asset>)) (+ a c))
+(define-method (- (a <asset>) (c <capital>)) (+ a (- c)))
+(define-method (- (c <capital>) (a <asset>)) (+ (- a) c))
+
+(define-method (+ (a1 <asset>) (a2 <asset>))
+  (apply + (cons a1 (capital a2))))
+
+(define-method (- (a1 <asset>) (a2 <asset>))
+  (+ a1 (- a2)))
+
 
 (define-class <transaction> ()
   (date #:init-keyword #:date
@@ -83,4 +128,4 @@
 										   account))
 					   transaction-list)))
 	  (- (apply + (map capital tos))
-	  	 (apply + (map capital froms))))))
+		 (apply + (map capital froms))))))
